6167ea6bd3cfb9d5bf46277d6b2f259b81005375
AlexMuia31 <alexmuia31@gmail.com>
2022-04-27 12:31:50 +0300

    contract deployed

--- contracts/RoboPunks.sol
+++ contracts/RoboPunks.sol
@@ -2,8 +2,8 @@
 
 pragma solidity ^0.8.4;
 
-import "@openzepplin/contracts/token/ERC721/ERC721.sol"; // importing a contract that we use for minting
-import "@openzepplin/contracts/access/Ownable.sol"; //allow function definition for the owner
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol"; // importing a contract that we use for minting
+import "@openzeppelin/contracts/access/Ownable.sol"; //allow function definition for the owner
 
 contract RoboPunksNFT is ERC721, Ownable {
     uint256 public mintPrice;
