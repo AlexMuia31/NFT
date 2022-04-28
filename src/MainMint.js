c0c6ce9f9392aeabdf92b4c114f410111ce1ea0b
AlexMuia31 <alexmuia31@gmail.com>
2022-04-28 15:22:21 +0300

    final

--- src/MainMint.js
+++ src/MainMint.js
@@ -1,5 +1,15 @@
 import React, { useState } from "react";
 import { ethers, BigNumber } from "ethers";
+import {
+  Box,
+  Button,
+  Flex,
+  Image,
+  Input,
+  Link,
+  Spacer,
+  Text,
+} from "@chakra-ui/react";
 import roboPunksNFT from "./RoboPunksNFT.json";
 
 const roboPunksNFTAddress = "0x2819751B598f00Ea50325aC574A0e937EBE75f9F";
@@ -18,7 +28,10 @@ const MainMint = ({ accounts, setAccounts }) => {
         signer
       );
       try {
-        const response = await contract.mint(BigNumber.from(mintAmount));
+        const response = await contract.mint(BigNumber.from(mintAmount), {
+          value: ethers.utils.parseEther((0.02 * mintAmount).toString()),
+        });
+
         console.log("response: ", response);
       } catch (err) {
         console.log("error: ", err);
@@ -35,25 +48,80 @@ const MainMint = ({ accounts, setAccounts }) => {
   };
 
   return (
-    <div>
-      <h1>RoboPunks</h1>
-      <p>
-        Its 2022 and You should mint some NFTs or Will send RoboPunk to come and
-        get you. This is not a threat!
-      </p>
-      {isConnected ? (
+    <Flex justify="center" align="center" height="100vh" paddingBottom="150px">
+      <Box width="520px">
         <div>
-          <div>
-            <button onClick={handleDecrement}>-</button>
-            <input type="number" value={mintAmount} />
-            <button onClick={handleIncrement}>+</button>
-          </div>
-          <button onClick={handleMint}>Mint Now</button>
+          <Text fontSize="48px" textShadow="0 5px #000000">
+            RoboPunks
+          </Text>
+
+          <Text
+            fontSize="30px"
+            letterSpacing="-5.5%"
+            fontFamily="VT323"
+            textShadow="0 2px 2px #000000"
+          >
+            Its 2022 and You should mint some NFTs or Will send RoboPunk to come
+            and get you. This is not a threat!
+          </Text>
         </div>
-      ) : (
-        <p>You must be connected to Mint.</p>
-      )}
-    </div>
+        {isConnected ? (
+          <div>
+            <Flex align="center" justify="center">
+              <Button
+                backgroundColor="#D6517D"
+                borderRadius="5px"
+                boxShadow="0px 2px 2px 1px #0F0F0F"
+                color="white"
+                fontFamily="inherit"
+                padding="15px"
+                marginTop="10px"
+                onClick={handleDecrement}
+              >
+                -
+              </Button>
+              <Input
+                readOnly
+                fontFamily="inherit"
+                width="100px"
+                height="40px"
+                textAlign="center"
+                paddingLeft="19px"
+                marginTop="10px"
+                type="number"
+                value={mintAmount}
+              />
+              <Button
+                backgroundColor="#D6517D"
+                borderRadius="5px"
+                boxShadow="0px 2px 2px 1px #0F0F0F"
+                color="white"
+                fontFamily="inherit"
+                padding="15px"
+                marginTop="10px"
+                onClick={handleIncrement}
+              >
+                +
+              </Button>
+            </Flex>
+            <Button
+              backgroundColor="#D6517D"
+              borderRadius="5px"
+              boxShadow="0px 2px 2px 1px #0F0F0F"
+              color="white"
+              fontFamily="inherit"
+              padding="15px"
+              marginTop="10px"
+              onClick={handleMint}
+            >
+              Mint Now
+            </Button>
+          </div>
+        ) : (
+          <p>You must be connected to Mint.</p>
+        )}
+      </Box>
+    </Flex>
   );
 };
 
--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import { Box, Button, Flex, Image, Link, Spacer } from "@chakra-ui/react";
+import Facebook from "./assets/social-media-icons/facebook_32x32.png";
+import Twitter from "./assets/social-media-icons/twitter_32x32.png";
+import Email from "./assets/social-media-icons/email_32x32.png";
 
 const NavBar = ({ accounts, setAccounts }) => {
   const isConnected = Boolean(accounts[0]);
@@ -13,20 +17,45 @@ const NavBar = ({ accounts, setAccounts }) => {
   }
 
   return (
-    <div>
-      <div>Facebook</div>
-      <div>Twitter</div>
-      <div>Email</div>
-      <div>About</div>
-      <div>Mint</div>
-      <div>Team</div>
-      {/* Connect*/}
-      {isConnected ? (
-        <p>Connected</p>
-      ) : (
-        <button onClick={connectAccount}>Connect</button>
-      )}
-    </div>
+    <Flex justify="space-between" align="center" padding="30px">
+      <Flex justify="space-around" width="40%" padding="0 75px">
+        <Link href="https://www.facebook.com" target="_blank">
+          <Image src={Facebook} boxSize="42px" margin="0 15px" />
+        </Link>
+        <Link href="https://twitter.com/Kenyan_Dev" target="_blank">
+          <Image src={Twitter} boxSize="42px" margin="0 15px" />
+        </Link>
+        <Link href="https://www.gmail.com" target="_blank">
+          <Image src={Email} boxSize="42px" margin="0 15px" />
+        </Link>
+      </Flex>
+      <Flex justify="space-around" align="center" width="40%" padding="30px">
+        <Box margin="0 15px">About</Box>
+        <Spacer />
+        <Box margin="0 15px">Mint</Box>
+        <Spacer />
+        <Box margin="0 15px">Team</Box>
+        <Spacer />
+        {/* Connect*/}
+        {isConnected ? (
+          <Box margin="0 15px">Connected</Box>
+        ) : (
+          <Button
+            backgroundColor="#D6517D"
+            borderRadius="5px"
+            boxShadow="0px 2px 2px 1px #0F0F0F"
+            color="white"
+            cursor="pointer"
+            fontFamily="inherit"
+            padding="15px"
+            margin="0 15px"
+            onClick={connectAccount}
+          >
+            Connect
+          </Button>
+        )}
+      </Flex>
+    </Flex>
   );
 };
 
